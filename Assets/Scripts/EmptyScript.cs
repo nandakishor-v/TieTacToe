abb57ae3dc13743c5b540e5f5b1738570c1f3191
Nandakishor.V <nandakishor.v2020@vitbhopal.ac.in>
2023-08-10 01:05:55 +0530

    gamebasic

---
+++ Assets/Scripts/EmptyScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmptyScript : MonoBehaviour
+{
+    public int id;
+    public GameObject camera;
+
+    private void OnMouseDown()
+    {
+        camera.GetComponent<GameScript>().Spawn(this.gameObject, id);
+    }
+}
--- Assets/Scripts/GameScript.cs
+++ Assets/Scripts/GameScript.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public string gameMode;
+    public GameObject cross, nought;
+    public TextMeshProUGUI Instructions;
+    public enum Seed { EMPTY, CROSS, NOUGHT };
+    public Seed Turn;
+    private void Awake()
     {
-        
+        // to get the Game mode information from the previous scene
+        /*GameObject persistantObj = GameObject.FindGameObjectWithTag("PersistantObj") as GameObject;
+        gameMode = persistantObj.GetComponent<PersistanceScript>().gameMode;
+        Destroy(persistantObj);*/
+        // here turn is player 1 which is cross
+        Turn = Seed.CROSS;
+        // Set initial instruction
+        Instructions.text = "Turn: Player 1";
     }
-
-    // Update is called once per frame
-    void Update()
+    public void Spawn(GameObject emptycell, int id)
     {
-        
+        // conditions to spawn cross or nought
+        if (Turn == Seed.CROSS)
+        {
+             Instantiate(cross, emptycell.transform.position, Quaternion.identity);
+            Turn = Seed.NOUGHT;
+            Instructions.text = "Turn: Player 2";
+
+        }
+       else if (Turn == Seed.NOUGHT)
+        {
+            Instantiate(nought, emptycell.transform.position, Quaternion.identity);
+            Turn = Seed.CROSS;
+            Instructions.text = "Turn: Player 1";
+        }
+        Destroy(emptycell);
+
     }
 }
+
